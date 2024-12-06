cd7f1aea8efd65ac61fb64cb892f152da4c17cb8
MaterialShadow <1414240867@qq.com>
2024-12-06 12:15:17 +0800

    优化解密逻辑

--- cmd/file.go
+++ cmd/file.go
@@ -116,6 +116,7 @@ var addCmd = &cobra.Command{
 			}
 			// 如果读取到文件末尾，则退出循环
 			if bytesRead == 0 {
+				p.Shutdown()
 				break
 			}
 			// 将数据写入目标文件
@@ -166,11 +167,13 @@ var recoverCmd = &cobra.Command{
 	Short: "恢复文件",
 	Long:  `恢复文件`,
 	Run: func(cmd *cobra.Command, args []string) {
+		logrus.Info("开始解密加密文件")
 		if len(args) < 1 {
 			logrus.Error("请输入文件路径")
 			return
 		}
 		fp := args[0]
+		logrus.Info("加密文件路径:", fp)
 		FileInfo, err := os.Stat(fp)
 		if os.IsNotExist(err) {
 			logrus.Error("文件不存在")
@@ -196,37 +199,95 @@ var recoverCmd = &cobra.Command{
 		}
 		logrus.Info("解密文件名:", cipherFileName)
 
+		// 保存解密文件
+		output := cmd.Flags().Lookup("output").Value.String()
+		if output == "" {
+			output = filepath.Join(filepath.Dir(fp), filepath.Base(cipherFileName))
+			logrus.Info("未指定输出文件保存路径，使用默认路径:", output)
+		} else {
+			outputFileInfo, err := os.Stat(output)
+			if err != nil {
+				if os.IsNotExist(err) {
+					output = filepath.Join(output, filepath.Base(cipherFileName))
+				} else {
+					logrus.Info("指定输出路径错误")
+					return
+				}
+			}
+			if outputFileInfo.IsDir() {
+				output = filepath.Join(output, filepath.Base(cipherFileName))
+			}
+			output, err = filepath.Abs(output)
+			if err != nil {
+				logrus.Info("指定输出路径错误")
+				return
+			}
+			logrus.Info("恢复文件保存路径:", output)
+		}
+		_, err = os.Stat(output)
+		if err == nil {
+			logrus.Info("输出文件已存在，请先删除文件或指定其他路径")
+			return
+		}
+
+		logrus.Info("开始解密文件")
 		// 解密文件
 		cipherData, err := utils.CipherInstance.RcCipher.DecryptData(file)
 		if err != nil {
 			logrus.Info("解密文件失败")
 			return
 		}
-		// 保存解密文件
-		output := cmd.Flags().Lookup("output").Value.String()
-		if output == "" {
-			output = filepath.Join(filepath.Dir(fp), filepath.Base(cipherFileName))
-		} else {
-			output, err = filepath.Abs(output)
-			if err != nil {
-				logrus.Info("指定输出路径错误")
-				return
-			}
-		}
-		logrus.Info("恢复文件保存路径:", output)
+		logrus.Info("解密文件成功")
+
+		logrus.Info("开始写入解密文件")
 		outputFile, err := os.Create(output)
 		if err != nil {
 			logrus.Info("创建解密文件失败")
 			return
 		}
 		defer outputFile.Close()
-		// 将解密数据写入解密文件
-		bytes, err := io.ReadAll(cipherData)
-		if err != nil {
-			logrus.Info("写入解密文件失败")
-			return
+
+		// 创建一个新的mpb实例
+		p := mpb.New(mpb.WithWidth(64))
+
+		// 创建一个新的进度条
+		bar := p.New(FileInfo.Size(),
+			// BarFillerBuilder with custom style
+			mpb.BarStyle().Lbound("╢").Filler("▌").Tip("▌").Padding("░").Rbound("╟"),
+			mpb.PrependDecorators(
+				// display our name with one space on the right
+				decor.Name("写入文件", decor.WC{C: decor.DindentRight | decor.DextraSpace}),
+				// replace ETA decorator with "done" message, OnComplete event
+				decor.OnComplete(decor.AverageETA(decor.ET_STYLE_GO), "完成"),
+			),
+			mpb.AppendDecorators(decor.Percentage()),
+		)
+
+		// 创建一个缓冲区
+		buffer := make([]byte, 32*1024) // 32KB
+		for {
+			// 从源文件中读取数据
+			bytesRead, err := cipherData.Read(buffer)
+			if err != nil && err != io.EOF {
+				logrus.Error("Error reading file:", err)
+				return
+			}
+			// 如果读取到文件末尾，则退出循环
+			if bytesRead == 0 {
+				p.Shutdown()
+				break
+			}
+			// 将数据写入目标文件
+			_, err = outputFile.Write(buffer[:bytesRead])
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				return
+			}
+			// 更新进度条
+			bar.IncrInt64(int64(bytesRead))
 		}
-		outputFile.Write(bytes)
+		p.Wait()
+		logrus.Info("写入解密文件完成")
 
 	},
 }
